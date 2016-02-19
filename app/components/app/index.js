6b7b29d827c8f64f4fda708ed37a0bcd16444b47
Brandon <brandilla1337@gmail.com>
2016-02-19 01:38:44 -0500

    Skeleton files added

---
+++ app/components/app/index.js
@@ -0,0 +1,24 @@
+import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router';
+import style from './style.scss';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Imgur React</h1>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                </ul>
+
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+App.propTypes = {
+    children: PropTypes.node
+};
+
+export default App;
---
+++ webpack.config.babel.js
@@ -0,0 +1,58 @@
+import path from 'path';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+
+let devConfig = {
+    context: path.join(__dirname, 'app'),
+    entry: [
+        './app.js'
+    ],
+    output: {
+        path: path.join(__dirname, 'build/assets/js'),
+        publicPath: 'assets/js/',
+        filename: 'app.js',
+    },
+    devtool: 'eval-source-map',
+    devServer: {
+        contentBase: 'public',
+        historyApiFallback: false
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                loaders: ['style', 'css']
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                loaders: ['style', 'css', 'sass']
+            },
+            {
+                test: /\.(jpg|png|ttf|eot|woff|woff2|svg)$/,
+                exclude: /node_modules/,
+                loader: 'url?limit=100000'
+            }
+        ]
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            { 
+                from: '../public', 
+                to: '/build' 
+            }
+        ])
+    ]
+}
+
+if (process.env.NODE_ENV === 'production') {
+    devConfig.devtool = '';
+    devConfig.devServer = {};
+};
+
+export default devConfig;
